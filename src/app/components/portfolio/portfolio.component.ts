fd48829612d5ca807aa2848bcb3770ba8f263d6b
praveenchandkasarla <30707856+praveenchandkasarla@users.noreply.github.com>
2017-08-11 13:50:33 -0600

    Add files via upload

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Ng2parallax } from '../../directives/parallax.directive';
+declare function init(domObj:any):any;
 
 @Component({
     selector: 'portfolio',
     templateUrl: './portfolio.component.html'
 })
 
-export class PortfolioComponent {
-    constructor(private _router: Router) {   
+export class PortfolioComponent implements OnInit {
+    public camera: any;
+    public scene: any;
+    public renderer: any;
+    public mesh: any;
+    public appendElement:number;
+    @ViewChild('cube') cube: any;
+    constructor(private _router: Router,private el: ElementRef) {
+    }
+    public ngOnInit() {
+        console.log('Initial App State'+this.cube.nativeElement);   
+        init(this.cube.nativeElement);    
     }
 }
